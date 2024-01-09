cde0835f335905809e78482954e75abcdcf509b8
Tochukwu Odoh <cashearncrypto@gmail.com>
2024-01-09 01:49:21 +0000

    Set data structure

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Odoh Tochukwu <https://github.com/odoh-tc>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
